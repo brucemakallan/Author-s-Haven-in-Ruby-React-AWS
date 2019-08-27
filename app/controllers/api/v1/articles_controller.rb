24e601d5ba05384e39fba2f20aad5ac1442014cf
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-27 19:17:36 +0300

    Fix rubocop errors and warnings

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,23 +1,26 @@
-class Api::V1::ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-    render json: @articles, status: :ok
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        @articles = Article.all
+        render json: @articles, status: :ok
+      end
+
+      # def create
+
+      # end
+
+      # def show
+
+      # end
+
+      # def update
+
+      # end
+
+      # def destroy
+
+      # end
+    end
   end
-  
-  # def create
-    
-  # end
-  
-  # def show
-    
-  # end
-  
-  # def update
-    
-  # end
-  
-  # def destroy
-    
-  # end
-  
 end
