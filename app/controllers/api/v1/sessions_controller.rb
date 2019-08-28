f413d5cf441ba91c0ea5a52554ae4720ee8c1ec6
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-28 23:31:16 +0300

    Add session token - returned after Sign-in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ module Api
       def create
         user = User.where(email: params[:email]).first
         if user&.valid_password?(params[:password])
-          render json: user.as_json(only: %i[id email]), status: :created
+          render json: user.as_json(only: %i[id email authentication_token]), status: :created
         else
           render json: { error: INVALID_CREDENTIALS }, status: :unauthorized
         end
