f1f9017e40136e3e409636598f90136a4c2809f5
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-28 15:07:02 +0300

    Show validation errors for Create and Edit Article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+      INVALID_INPUT = 'Invalid Input'.freeze
+
       def index
         @articles = Article.all
         render json: @articles, status: :ok
@@ -8,8 +10,12 @@ module Api
 
       def create
         @article = Article.new(article_params)
-        @article.save
-        render json: @article, status: :created
+        if @article.valid?
+          @article.save
+          render json: @article, status: :created
+        else
+          render_error(INVALID_INPUT)
+        end
       end
 
       def show
@@ -20,7 +26,11 @@ module Api
       def update
         @article = Article.find(params[:id])
         @article.update(article_params)
-        render json: @article, status: :ok
+        if @article.valid?
+          render json: @article, status: :ok
+        else
+          render_error(INVALID_INPUT)
+        end
       end
 
       def destroy
@@ -33,6 +43,10 @@ module Api
 
       def article_params
         params.require(:article).permit(:title, :body, :image_url)
+      end
+
+      def render_error(message)
+        render json: { error: message, details: @article.errors }, status: :bad_request
       end
     end
   end
