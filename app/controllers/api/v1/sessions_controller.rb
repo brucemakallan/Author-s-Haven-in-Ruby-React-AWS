5dc6b6e8e4b932fa536f3c0e031127a6854bedf9
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-29 12:02:38 +0300

    Using JBuilder for custom JSON responses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,9 +4,9 @@ module Api
       INVALID_CREDENTIALS = 'Invalid credentials'.freeze
 
       def create
-        user = User.where(email: params[:email]).first
-        if user&.valid_password?(params[:password])
-          render json: user.as_json(only: %i[id email authentication_token]), status: :created
+        @user = User.where(email: params[:email]).first
+        if @user&.valid_password?(params[:password])
+          render :create, status: :created
         else
           render json: { error: INVALID_CREDENTIALS }, status: :unauthorized
         end
