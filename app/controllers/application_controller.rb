cd3eb4b320b5433960aea729f51bc63536aaccdf
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-30 20:32:22 +0300

    Fix Rubocop warnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,8 @@ class ApplicationController < ActionController::API
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: %i[first_name last_name email password password_confirmation]
+    )
   end
 end
