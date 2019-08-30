112b0a1a2b7bc302298911b0b3ffa77066deb6ad
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-30 23:28:44 +0300

    Add users namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      devise_for :users,
-                 path: '',
-                 path_names: {
-                   sign_in: 'login',
-                   sign_out: 'logout',
-                   registration: 'signup'
-                 },
-                 controllers: {
-                   sessions: 'api/v1/users/sessions',
-                   registrations: 'api/v1/users/registrations'
-                 }      
-
       resources :articles
+
+      namespace :users, defaults: { format: :json } do
+        devise_for :users,
+                   path: '',
+                   path_names: {
+                     sign_in: 'login',
+                     sign_out: 'logout',
+                     registration: 'signup'
+                   },
+                   controllers: {
+                     sessions: 'api/v1/users/sessions',
+                     registrations: 'api/v1/users/registrations'
+                   }      
+      end
     end
   end
 end
